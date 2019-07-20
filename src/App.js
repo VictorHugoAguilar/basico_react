f2658ec6dbf0405737cae7a7d1c1dd44c74b7df8
victor hugo <victor03016@gmail.com>
2019-07-20 18:34:32 +0200

    Creacion Componente
    Creacion del primer componente

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React, { fragment } from "react";
 import "./App.css";
+import PrimerComponente from './components/PrimerComponente';
 
 function App() {
-    // Añadimos variables
-
-    const empleado = {
-        nombre: "Victor",
-        trabajo: "Desarrollo de app"
-    };
 
     return (
-        // Siempre hay que encapsular todo en un solo elemento por ejemplo
-        //<div>
-        //  <h1>{empleado.nombre}</h1>
-        //  <h2>{empleado.trabajo}</h2>
-        //</div>
-
-        // la alternativa es fragment que es un contenedor de elemetos
-        <fragment>
-            <h1>{empleado.nombre}</h1>
-            <h2>{empleado.trabajo}</h2>
-        </fragment>
+       <PrimerComponente></PrimerComponente>
     );
 }
 
---
+++ src/components/PrimerComponente.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class PrimerComponente extends Component {
+    render() {
+        return (<h1>Hola Primer Component</h1>)
+    }
+}
+
+export default PrimerComponente;
