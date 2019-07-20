03096c8293930cc110fc5002545461731b70bb97
victor hugo <victor03016@gmail.com>
2019-07-20 18:24:14 +0200

    fragmen (contenedor de elementos)
    Para poder enviar varios elementos tenemos que encapsularlos ya sea en div o en estos fragment que tenemos que importar desde react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { fragment } from "react";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    // Añadimos variables
+
+    const empleado = {
+        nombre: "Victor",
+        trabajo: "Desarrollo de app"
+    };
+
+    return (
+        // Siempre hay que encapsular todo en un solo elemento por ejemplo
+        //<div>
+        //  <h1>{empleado.nombre}</h1>
+        //  <h2>{empleado.trabajo}</h2>
+        //</div>
+
+        // la alternativa es fragment que es un contenedor de elemetos
+        <fragment>
+            <h1>{empleado.nombre}</h1>
+            <h2>{empleado.trabajo}</h2>
+        </fragment>
+    );
 }
 
 export default App;
