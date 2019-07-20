88bc249525b479d89364ebf5b7bcd5b2abf222db
victor hugo <victor03016@gmail.com>
2019-07-20 18:54:46 +0200

    Creacion de Compoentes Header-Footer
    Pasamos datos entre componentes en el parametro de cada compoenente entre {} llamamos a los valores que introducimos como atributos de la etiquetas de los componentes.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { fragment } from "react";
 import "./App.css";
-import PrimerComponenteFuncional from './components/PrimerComponente';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
 
-    return (
-       <PrimerComponenteFuncional></PrimerComponenteFuncional>
-    );
+  const fecha = new Date ().getFullYear();
+
+  return (
+    <fragment>
+      <Header 
+        titulo='Tienda Virtual'
+      />
+
+      <Footer fecha={fecha}></Footer>
+    </fragment>
+  )
 }
 
 export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = ({fecha}) => {
+    return (
+        <footer>Toodos los derecho reservados © {fecha} </footer>
+    )
+}
+
+export default Footer;
